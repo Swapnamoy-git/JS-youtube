d841bbaaa02c478faecd78d84430fc22bb03dd46
Swapnamoy Midya <124113927+Swapnamoy-git@users.noreply.github.com>
2024-01-02 20:04:53 +0000

    datatypes and its conversions

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,12 @@
+const accountID = 144335
+let accountEmail = "swapnamoy@google.com"
+var accountPassword = "4355sw"
+accountcity = "Jaipur"
+
+// acoountID = "43" //not allowed
+accountEmail = "guess@google.com"
+accountPassword = "ojmaq"
+
+console.log(accountID);
+
+console.table([accountID, accountPassword, accountEmail])
---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,33 @@
+/*let score = "33"
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+
+let score2 = null
+console.log(typeof score2);
+
+let valueInNumber2 = Number(score2)
+console.log(typeof valueInNumber2);*/
+
+//"33" => 33
+// "33abc" => NaN
+//true => 1; false => 0
+
+//let isLoggedIn = "swap"
+
+//let bolleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(bolleanIsLoggedIn);
+
+//1 => true
+//0 => false
+//"" => false
+//"swap" => true
+
+let someNumber = 33
+
+let stringNumber = String (someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
